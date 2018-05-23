e966c1d21dd95a533ee79eb4aaf08dcacd9114a8
EC2 Default User <ec2-user@ip-172-31-26-2.us-east-2.compute.internal>
2018-05-23 14:10:14 +0000

    last commit

--- test/controllers/pins_controller_test.rb
+++ test/controllers/pins_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class PinsControllerTest < ActionDispatch::IntegrationTest
+class PinsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
